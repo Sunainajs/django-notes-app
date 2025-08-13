404e17072dc383a4aec78d839566d1fbc5ba2df3
Sunainajs <sunainajs0303@gmail.com>
2025-08-13 13:35:44 +0530

    admin.py

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-from .models import Note
+from .models import Note, NoteFile
 
-# Register your models here.
-admin.site.register(Note)
+# Keep your original Note registration
+class NoteFileInline(admin.TabularInline):
+    model = NoteFile
+    extra = 0
+
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "created")
+    inlines = [NoteFileInline]
+
+@admin.register(NoteFile)
+class NoteFileAdmin(admin.ModelAdmin):
+    list_display = ("id", "note", "file", "uploaded_at")
