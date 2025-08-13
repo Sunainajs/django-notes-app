c79a6bb9e317ad63412c4d8051daff1687ce42f1
Sunainajs <sunainajs0303@gmail.com>
2025-08-13 13:36:54 +0530

    serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,21 @@
-from rest_framework.serializers import ModelSerializer
-from .models import Note
+from rest_framework import serializers
+from .models import Note, NoteFile
 
-class NoteSerializer(ModelSerializer):
+# NEW: serializer for uploaded files
+class NoteFileSerializer(serializers.ModelSerializer):
+    filename = serializers.SerializerMethodField()
+
+    class Meta:
+        model = NoteFile
+        fields = ["id", "file", "filename", "uploaded_at"]
+
+    def get_filename(self, obj):
+        return obj.file.name.split('/')[-1]
+
+# UPDATE: NoteSerializer to include files
+class NoteSerializer(serializers.ModelSerializer):
+    files = NoteFileSerializer(many=True, read_only=True)
+
     class Meta:
         model = Note
-        fields = '__all__'
+        fields = ["id", "title", "body", "created", "updated", "files"]
